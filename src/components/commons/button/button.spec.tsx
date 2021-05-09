615e14e1e7befdf9cda5da3437abe50d9e5a7e5a
henrique <henriquecesarl@hotmail.com>
2021-05-09 13:10:02 -0300

    Fix button test

--- src/components/commons/button/button.spec.tsx
+++ src/components/commons/button/button.spec.tsx
@@ -4,7 +4,7 @@ import Button from './button'
 
 test('should get button by label text', () => {
   const { getByText } = render(
-    <Button variant="clear" label="Button" onClick={() => undefined} />
+    <Button label="Button" onClick={() => undefined} />
   )
 
   getByText('Button')
