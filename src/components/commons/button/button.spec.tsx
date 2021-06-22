1650e4547563b14d29ccc1205dbdc2c0500d21d4
henrique <henriquecesarl@hotmail.com>
2021-06-21 22:02:39 -0300

    Remove barrel

--- src/components/commons/button/button.spec.tsx
+++ src/components/commons/button/button.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import Button from './button'
+import Button from './index'
 
 test('should get button by label text', () => {
   const { getByText } = render(
